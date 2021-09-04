ed03b3b43685f4e35a57c056594f814540357fb9
soorajkumarluhana <soorajkumarluhana@gmail.com>
2021-09-04 20:28:29 +0500

    done

---
+++ chatapp/lib/Screens/auth.dart
@@ -0,0 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+class AuthMethods {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future signInWithEmailAndPassword(String email, String password) async {
+   
+  try {
+
+    
+        
+  } catch (e) {
+  }
+
+
+  }
+}
--- chatapp/lib/Screens/signup.dart
+++ chatapp/lib/Screens/signup.dart
@@ -1,6 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class Signup extends StatefulWidget {
